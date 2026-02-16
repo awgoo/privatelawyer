59cecf417ac463781735836689a0d58850ffa0bc
awgoo <etdmfinabdiel@gmail.com>
2026-02-16 11:51:14 +0700

    Update retry.py

--- utils/retry.py
+++ utils/retry.py
@@ -1,1 +1,17 @@
+import time
+from core.config import MAX_RETRIES
+from core.logger import logger
 
+def retry(func):
+
+    def wrapper(*args, **kwargs):
+        for attempt in range(MAX_RETRIES):
+            try:
+                return func(*args, **kwargs)
+            except Exception as e:
+                logger.warning(f"Retry {attempt+1} بسبب error: {e}")
+                time.sleep(1.5)
+
+        raise Exception("Max retries exceeded")
+
+    return wrapper
